c50c18af97b80860aa61c8ecbdd5f41f5862bb5e
jimmyff <jimmy@rocketware.co.uk>
2024-01-04 10:10:07 +0000

    Started expanding theme color options

--- deckorator/lib/src/theme.dart
+++ deckorator/lib/src/theme.dart
@@ -1,11 +1,28 @@
 class GameTheme {
-  final Map<String, String> colors;
+  /// Named colors
+  final Map<String, ThemeColor> colors;
 
-  String colorHex(String key) {
-    if (!colors.containsKey(key))
-      throw Exception('Color not found with key: $key');
-    return colors[key]!;
-  }
+  // String colorHex(String key) {
+  //   if (!colors.containsKey(key))
+  //     throw Exception('Color not found with key: $key');
+  //   return colors[key]!;
+  // }
 
   GameTheme({required this.colors});
+}
+
+class ThemeColor {
+  /// CMYK color, used for print output.
+  /// 0-100 values for cyan, magenta, yellow, key (black)
+  final (int, int, int, int) _cmyk;
+
+  /// RGB color, used for digital output
+  /// 0-255 values for red, green, blue
+  final (int, int, int) _rgb;
+
+  ThemeColor({
+    required (int, int, int, int)? cmyk,
+    required (int, int, int)? rgb,
+  })  : _cmyk = cmyk!,
+        _rgb = rgb!;
 }
